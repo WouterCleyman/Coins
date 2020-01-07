80466fd49c93f27265dc069cf8a2a299c1b5bba9
WouterCleyman <59503485+WouterCleyman@users.noreply.github.com>
2020-01-07 22:07:19 +0100

    Add files via upload

---
+++ ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    // Use this for initialization
+    public static ScoreManager instance;
+    public Text text;
+    int score;
+	void Start () {
+		if(instance == null)
+        {
+            instance = this;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void ChangeScore(int coinValue)
+    {
+        score += coinValue;
+        text.text = "X" + score.ToString();
+    }
+}
